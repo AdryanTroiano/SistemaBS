df7b7280b0ac15043efd822f4bdca937c819e3b1
Adryan <adryantroian@gmail.com>
2025-06-06 11:45:41 -0300

    adição da linha peso na tabela cadastrobs

--- cadastro.sql
+++ cadastro.sql
@@ -3,9 +3,9 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Tempo de geração: 29/05/2025 às 16:06
+-- Tempo de geração: 06/06/2025 às 16:44
 -- Versão do servidor: 10.4.32-MariaDB
--- Versão do PHP: 8.2.12
+-- Versão do PHP: 8.0.30
 
 SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
 START TRANSACTION;
@@ -41,26 +41,30 @@ CREATE TABLE `cadastrobs` (
   `bairro` varchar(50) NOT NULL,
   `nasc` date NOT NULL,
   `ts` varchar(4) NOT NULL,
-  `datedonation` date NOT NULL
+  `datedonation` date NOT NULL,
+  `peso` int(11) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 
 --
 -- Despejando dados para a tabela `cadastrobs`
 --
 
-INSERT INTO `cadastrobs` (`id`, `nome`, `sexo`, `cpf`, `telefone`, `email`, `endereco`, `numero`, `cep`, `complemento`, `bairro`, `nasc`, `ts`, `datedonation`) VALUES
-(1, 'José pereira', 'M', '123.456.789-10', '(12) 34567-8901', 'jose@gmail.com', 'Avenida Herculano Rangel', 12, '15902-024', '', 'Jardim Contendas', '1967-02-05', 'O+', '2024-11-05'),
-(2, 'Luis Almeida Campos', 'M', '123.443.323-33', '(34) 41332-3233', 'luiscampos@outlook.com', 'Praça Doutor Fued Simão', 67, '15904-012', '', 'Vila Nova', '1989-06-09', 'O+', '2024-08-05'),
-(3, 'Maria Clara', 'M', '031.302.323-31', '(16) 99323-3323', 'mariaclara@outlook.com', 'Avenida Brigadeiro Faria Lima', 12, '15090-000', '', 'Vila São José', '2000-08-20', 'O+', '2024-05-05'),
-(4, 'Leticia de Oliveira', 'F', '132.145.151-51', '(12) 42424-4444', 'leticia@gmail.com', 'RUA DOUTOR JOAQUIM MACHADO FARO ROLEMBERG', 37, '15903-020', '', 'Jardim São Luiz', '2003-09-22', 'O+', '2024-11-05'),
-(5, 'Rui da Silva', 'M', '141.924.924-94', '(16) 93929-3929', 'rui@gmail.com', 'Rua Dona Elvira Sargi Monteiro', 31, '15901-024', '', 'Jardim Alvorada', '1979-08-22', 'O+', '2024-10-27'),
-(6, 'Pintosvaldo da Silva', 'M', '482.884.244-24', '(16) 99233-2323', 'pintosvaldo@gmail.com', 'Avenida Vicente José Parise', 189, '15901-000', '', 'Vila Portuguesa', '1999-10-07', 'O+', '2024-01-05'),
-(7, 'Raimunda Souza', 'F', '131.231.231-23', '(91) 39123-9821', 'raimunda@gmail.com', 'Rua Doutor Manoel Fadigas de Souza', 123, '15901-020', '', 'Jardim Alvorada', '2000-11-28', 'O+', '2023-06-05'),
-(8, 'Roberto Felipe', 'M', '812.831.283-18', '(16) 99308-9219', 'roberto@gmail.com', 'Rua Visconde do Rio Branco', 37, '15900-015', '', 'Centro', '1980-02-20', 'O+', '2024-11-05'),
-(9, 'Roberto Souza', 'M', '131.231.313-13', '(31) 23131-3131', 'robertao@gmail.com', 'Praça Tókio', 12, '15904-021', '', 'Parque Residencial Laranjeiras I', '1959-11-05', 'O+', '2024-11-05'),
-(10, 'Deide Costa', 'F', '123.123.123-31', '(81) 38838-3232', 'deidecosta@gmail.com', 'Rua Dalmo Braga', 1244, '15902-044', '', 'Jardim Contendas', '1999-09-12', 'A+', '2024-11-05'),
-(11, 'Socorro da Silva', 'M', '184.848.448-48', '(16) 99323-3323', 'socorro@gmail.com', 'Rua Antonio Palópoli', 12, '15901-054', '', 'Vila Sargi', '1978-04-12', 'A-', '2024-11-05'),
-(12, 'JANAINA MAURA TROIANO', 'M', '132.322.131-23', '(16) 99308-9219', 'adryantroian@gmail.com', 'Rua Doutor Joaquim Machado Faro Rolemberg', 37, '15905-020', '', 'Jardim Bela Vista', '2024-11-28', 'O+', '2024-11-06');
+INSERT INTO `cadastrobs` (`id`, `nome`, `sexo`, `cpf`, `telefone`, `email`, `endereco`, `numero`, `cep`, `complemento`, `bairro`, `nasc`, `ts`, `datedonation`, `peso`) VALUES
+(1, 'José pereira', 'M', '123.456.789-10', '(12) 34567-8901', 'jose@gmail.com', 'Avenida Herculano Rangel', 12, '15902-024', '', 'Jardim Contendas', '1967-02-05', 'O+', '2024-06-12', 120),
+(2, 'Luis Almeida Campos', 'M', '123.443.323-33', '(34) 41332-3233', 'luiscampos@outlook.com', 'Praça Doutor Fued Simão', 67, '15904-012', '', 'Vila Nova', '1989-06-09', 'O+', '2024-08-05', 0),
+(3, 'Maria Clara', 'M', '031.302.323-31', '(16) 99323-3323', 'mariaclara@outlook.com', 'Avenida Brigadeiro Faria Lima', 12, '15090-000', '', 'Vila São José', '2000-08-20', 'O+', '2024-05-05', 0),
+(4, 'Leticia de Oliveira', 'F', '132.145.151-51', '(12) 42424-4444', 'leticia@gmail.com', 'RUA DOUTOR JOAQUIM MACHADO FARO ROLEMBERG', 37, '15903-020', '', 'Jardim São Luiz', '2003-09-22', 'O+', '2024-11-05', 0),
+(5, 'Rui da Silva', 'M', '141.924.924-94', '(16) 93929-3929', 'rui@gmail.com', 'Rua Dona Elvira Sargi Monteiro', 31, '15901-024', '', 'Jardim Alvorada', '1979-08-22', 'O+', '2024-10-27', 0),
+(6, 'Pintosvaldo da Silva', 'M', '482.884.244-24', '(16) 99233-2323', 'pintosvaldo@gmail.com', 'Avenida Vicente José Parise', 189, '15901-000', '', 'Vila Portuguesa', '1999-10-07', 'O+', '2024-01-05', 0),
+(7, 'Raimunda Souza', 'F', '131.231.231-23', '(91) 39123-9821', 'raimunda@gmail.com', 'Rua Doutor Manoel Fadigas de Souza', 123, '15901-020', '', 'Jardim Alvorada', '2000-11-28', 'O+', '2023-06-05', 0),
+(8, 'Roberto Felipe', 'M', '812.831.283-18', '(16) 99308-9219', 'roberto@gmail.com', 'Rua Visconde do Rio Branco', 37, '15900-015', '', 'Centro', '1980-02-20', 'O+', '2024-11-05', 0),
+(9, 'Roberto Souza', 'M', '131.231.313-13', '(31) 23131-3131', 'robertao@gmail.com', 'Praça Tókio', 12, '15904-021', '', 'Parque Residencial Laranjeiras I', '1959-11-05', 'O+', '2024-11-05', 0),
+(10, 'Deide Costa', 'F', '123.123.123-31', '(81) 38838-3232', 'deidecosta@gmail.com', 'Rua Dalmo Braga', 1244, '15902-044', '', 'Jardim Contendas', '1999-09-12', 'A+', '2024-11-05', 0),
+(11, 'Socorro da Silva', 'M', '184.848.448-48', '(16) 99323-3323', 'socorro@gmail.com', 'Rua Antonio Palópoli', 12, '15901-054', '', 'Vila Sargi', '1978-04-12', 'A-', '2024-11-05', 0),
+(12, 'JANAINA MAURA TROIANO', 'M', '132.322.131-23', '(16) 99308-9219', 'adryantroian@gmail.com', 'Rua Doutor Joaquim Machado Faro Rolemberg', 37, '15905-020', '', 'Jardim Bela Vista', '2024-11-28', 'O+', '2024-11-06', 0),
+(13, 'thais', 'F', '231.321.123-21', '(12) 13212-1213', 'sdsdasd@gmail.com', 'Rua Umbelina Cabral de Mello', 12, '15902-130', '', 'Jardim Taquarão II', '2025-06-06', 'N.A', '2025-06-06', 0),
+(14, 'luis', 'M', '515.123.213-12', '(16) 99933-2323', 'luis@gmail.com', 'Rua Doutor Joaquim Machado Faro Rolemberg', 35, '15905-020', '', 'Jardim Bela Vista', '2025-06-06', 'N.A', '2025-06-06', 0),
+(15, 'adryan', 'M', '213.131.232-13', '(12) 32121-33', 'adryantroian@gmail.com', 'Rua Professor Luiz Antônio Fragoso', 12, '15902-020', '', 'Jardim Contendas', '2025-06-13', 'N.A', '2025-06-06', 50);
 
 -- --------------------------------------------------------
 
@@ -82,7 +86,7 @@ INSERT INTO `estoque_sangue` (`tipo_sangue`, `quantidade`) VALUES
 ('A-', 10),
 ('AB+', 11),
 ('AB-', 12),
-('B+', 4),
+('B+', 5),
 ('B-', 2),
 ('O+', 20),
 ('O-', 1);
@@ -140,7 +144,7 @@ ALTER TABLE `usuarios`
 -- AUTO_INCREMENT de tabela `cadastrobs`
 --
 ALTER TABLE `cadastrobs`
-  MODIFY `id` int(10) UNSIGNED NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=13;
+  MODIFY `id` int(10) UNSIGNED NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=16;
 
 --
 -- AUTO_INCREMENT de tabela `usuarios`
